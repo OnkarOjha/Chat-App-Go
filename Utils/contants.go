8fff9584813df2cf3765b59afe91995267d37346
Onkar Ojha <97879633+OnkarOjha@users.noreply.github.com>
2023-04-05 09:51:35 +0530

    Update contants.go

--- Utils/contants.go
+++ Utils/contants.go
@@ -12,10 +12,10 @@ const User = "postgres"
 const Password = "Onkar17082001"
 const Dbname = "chat_app"
 
-// // twilio credentials
-const TWILIO_ACCOUNT_SID string = "AC5869424c6ae2d0b27f66a5d5b9b90485"
-const TWILIO_AUTH_TOKEN string = "80c7112f1d2297548a6caec62c8bab81"
-const VERIFY_SERVICE_SID string = "VA6602f8535f8f1369b0ed68eed5d6af67"
+// // // twilio credentials
+// const TWILIO_ACCOUNT_SID string = "AC5869424c6ae2d0b27f66a5d5b9b90485"
+// const TWILIO_AUTH_TOKEN string = "80c7112f1d2297548a6caec62c8bab81"
+// const VERIFY_SERVICE_SID string = "VA6602f8535f8f1369b0ed68eed5d6af67"
 
 // JWT secret key
 var JwtKey = []byte("My_Key")
