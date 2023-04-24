1da2140c6853e881435d62bba77cf23183713eef
OnkarOjha <1908288@sbsstc.ac.in>
2023-04-24 10:30:46 +0530

    swagger changes with ec2

--- docs/docs.go
+++ docs/docs.go
@@ -651,7 +651,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0.0",
-	Host:             "localhost:8000",
+	Host:             "localhost:3000",
 	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "Chat-Application",
